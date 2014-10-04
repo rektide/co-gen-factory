ea567f2a362d7d14ebc32cd2069db09c5270cbf0
rektide <rektide@voodoowarez.com>
2014-10-04 15:13:45 -0400

    Allow a Generator generating Function as seed.

--- co-gen-factory.js
+++ co-gen-factory.js
@@ -1,12 +1,20 @@
 var memoizee= require('memoizee'),
   memoizeeMethods= require('memoizee/methods')
 
+
+var Generator = typeof GeneratorFunction !== 'undefined' ? GeneratorFunction : (function*(){yield undefined}).constructor
+
 /**
   Create a factory 
 */
 function factoryGen(_gen, _opts){
 	var memoized= memoize(function(){
-		return _gen.apply(this, arguments)
+		var gen= _gen.apply(this, arguments)
+		if(gen instanceof Generator){
+			return gen
+		}else if(gen instanceof Function){
+			return gen.apply(this, arguments)
+		}
 	}, _opts)
 
 	function *factoryGen(){
@@ -14,7 +22,7 @@ function factoryGen(_gen, _opts){
 			var iter = memoized.apply(this, arguments)
 			if(!iter || !iter.next){
 				memoized.delete.apply(memoized, arguments)
-				yield null
+				yield undefined
 				continue
 			}
 
